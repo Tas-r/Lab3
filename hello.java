e6fb1fa22da30f03c8923c9140fb7451f8aea659
♡ vi ♡ <121645437+mousecarpone@users.noreply.github.com>
2025-02-17 16:21:37 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main( String args[] )
     {
-        System.out.println( "Hello CSCI 4485" );
+        System.out.println( "hai!!!!! :3c" );
     }
 }
 

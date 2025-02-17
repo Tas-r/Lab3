267c8311b9b8c3a17ea408eda3048904d921350c
♡ vi ♡ <121645437+mousecarpone@users.noreply.github.com>
2025-02-17 15:54:30 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main( String args[] )
     {
-        System.out.println( "Hello, " + args[0] + "!" );
+        System.out.println( "Hi Tasnim!! We're cookin!!!! Wow!!" );
     }
 }
 
